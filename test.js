775ce4b85c43b34c297c7f46838d131c23ad6352
Sign <ccx01@126.com>
2013-08-29 20:29:33 +0800

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-{ "people": [
-  { "firstName": "Brett", "lastName":"McLaughlin", "email": "brett@newInstance.com" },
-  { "firstName": "Jason", "lastName":"Hunter", "email": "jason@servlets.com" },
-  { "firstName": "Elliotte", "lastName":"Harold", "email": "elharo@macfaq.com" }
-]}
